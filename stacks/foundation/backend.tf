085472850e47301808c093a20458118530a07efd
MalcWarren22 <mwarrenn02@gmail.com>
2025-12-22 19:56:49 -0500

    Initial commit: DevSecOps Netflix Terraform baseline

--- stacks/foundation/backend.tf
+++ stacks/foundation/backend.tf
@@ -1,6 +1,17 @@
 #Backend file for Terraform
 
+#Backend file for Terraform
+
 terraform {
+  required_version = "~> 1.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "devsecops-netflix-dev-tfstate-tfs3"
     key            = "foundation/terraform.tfstate"
