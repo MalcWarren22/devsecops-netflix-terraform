2a6fb78dc02348e2c0c0fe8d302652b0b577e54a
MalcWarren22 <mwarrenn02@gmail.com>
2025-12-22 20:11:15 -0500

    Initial commit: DevSecOps Netflix Terraform baseline

--- stacks/tools-ec2/main.tf
+++ stacks/tools-ec2/main.tf
@@ -1,20 +1,5 @@
 # Terraform Main file for tools-ec2 stack
 
-data "aws_ami" "ubuntu_2204" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_instance" "tools" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
